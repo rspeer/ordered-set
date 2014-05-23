a18dc1abe7bd792eb2f45d821748fbcc922e5291
Rob Speer <rob@luminoso.com>
2014-05-23 14:11:11 -0400

    Fix PyPI URL, bump version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="ordered-set",
-    version = '1.1',
+    version = '1.2',
     maintainer='Luminoso Technologies, Inc.',
     maintainer_email='rob@luminoso.com',
     license = "MIT-LICENSE",
-    url = 'http://github.com/LuminosoInsight/orderedset',
+    url = 'http://github.com/LuminosoInsight/ordered-set',
     platforms = ["any"],
     description = "A MutableSet that remembers its order, so that every entry has an index.",
     py_modules=['ordered_set'],
