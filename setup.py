23f8f67fa801e21b5fa927d73f2fa111fc70cdf3
Rob Speer <rob@luminoso.com>
2015-04-21 14:56:09 -0400

    add pypy3 to tox; add Trove classifiers

--- setup.py
+++ setup.py
@@ -10,4 +10,13 @@ setup(
     platforms = ["any"],
     description = "A MutableSet that remembers its order, so that every entry has an index.",
     py_modules=['ordered_set'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ]
 )
