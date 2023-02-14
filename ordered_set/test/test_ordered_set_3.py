c5bcb10a6445c35016acd7538eb31349323455f3
Idan Miara <idan@miara.com>
2023-02-14 12:23:52 +0200

    test/test_ordered_set_3.py - update the tests from https://github.com/bustawin/ordered-set-37/blob/master/test/test_testing.py to test against multiple set types

--- ordered_set/test/test_ordered_set_3.py
+++ ordered_set/test/test_ordered_set_3.py
@@ -1,44 +1,55 @@
+# The following tests are based on
+# https://github.com/bustawin/ordered-set-37/blob/master/test/test_testing.py
+# Thus shall be under the license:
+# https://github.com/bustawin/ordered-set-37/blob/master/LICENSE.md
+
 from pathlib import Path
 
 import mypy.api
 import pytest
 
-from ordered_set import OrderedSet
+from ordered_set.test import all_sets, stable_and_ordered_sets
 
 TESTS = Path(__file__).parent
 
 
-def test_add():
-    x = OrderedSet([1, 2, -1, "bar"])
+@pytest.mark.parametrize("set_t", stable_and_ordered_sets)
+def test_add(set_t):
+    x = set_t([1, 2, -1, "bar"])
     x.add(0)
     assert list(x) == [1, 2, -1, "bar", 0]
 
 
-def test_discard():
-    x = OrderedSet([1, 2, -1])
+@pytest.mark.parametrize("set_t", all_sets)
+def test_discard(set_t):
+    x = set_t([1, 2, -1])
     x.discard(2)
     assert list(x) == [1, -1]
 
 
-def test_discard_ignores_missing_element():
-    x = OrderedSet()
+@pytest.mark.parametrize("set_t", all_sets)
+def test_discard_ignores_missing_element(set_t):
+    x = set_t()
     x.discard(1)  # This does not raise
 
 
-def test_remove():
-    x = OrderedSet([1])
+@pytest.mark.parametrize("set_t", all_sets)
+def test_remove(set_t):
+    x = set_t([1])
     x.remove(1)
     assert not x
 
 
-def test_remove_raises_missing_element():
-    x = OrderedSet()
+@pytest.mark.parametrize("set_t", all_sets)
+def test_remove_raises_missing_element(set_t):
+    x = set_t()
     with pytest.raises(KeyError):
         x.remove(1)
 
 
-def test_getitem():
-    x = OrderedSet([1, 2, -1])
+@pytest.mark.parametrize("set_t", stable_and_ordered_sets)
+def test_getitem(set_t):
+    x = set_t([1, 2, -1])
     assert x[0] == 1
     assert x[1] == 2
     assert x[2] == -1
@@ -46,43 +57,58 @@ def test_getitem():
         x[3]
 
 
-def test_len():
-    x = OrderedSet([1])
+@pytest.mark.parametrize("set_t", all_sets)
+def test_len(set_t):
+    x = set_t([1])
     assert len(x) == 1
 
 
-def test_iter():
-    for x in OrderedSet([1]):
+@pytest.mark.parametrize("set_t", all_sets)
+def test_iter(set_t):
+    for x in set_t([1]):
         assert x == 1
 
 
-def test_str():
-    x = OrderedSet([1, 2, 3])
-    assert str(x) == "{1, 2, 3}"
+@pytest.mark.parametrize("set_t", stable_and_ordered_sets)
+def test_str(set_t):
+    x = set_t([1, 2, 3])
+    assert str(x) == f"{set_t.__name__}([1, 2, 3])"
 
 
-def test_repr():
-    x = OrderedSet([1, 2, 3])
-    assert repr(x) == "<OrderedSet {1, 2, 3}>"
+@pytest.mark.parametrize("set_t", stable_and_ordered_sets)
+def test_repr(set_t):
+    x = set_t([1, 2, 3])
+    assert str(x) == f"{set_t.__name__}([1, 2, 3])"
 
 
-def test_eq():
-    x = OrderedSet([1, 2, 3])
-    y = OrderedSet([1, 2, 3])
+@pytest.mark.parametrize("set_t", all_sets)
+def test_eq(set_t):
+    x = set_t([1, 2, 3])
+    y = set_t([1, 2, 3])
     assert x == y
     assert x is not y
 
 
-def test_init_empty():
-    x = OrderedSet()
+@pytest.mark.parametrize("set_t", all_sets)
+def test_init_empty(set_t):
+    x = set_t()
     assert len(x) == 0
     x.add(2)
     assert len(x) == 1
 
 
-def test_typing_mypy():
+@pytest.mark.skip("Not ready yet")
+@pytest.mark.parametrize("set_t", all_sets)
+def test_typing_mypy(set_t, do_assert: bool = True):
     """Checks the typing values with mypy."""
     fixture = TESTS / "_test_mypy.py"
-    module = TESTS.parent / "ordered_set_37"
+    module = TESTS.parent / "ordered_sets"
     *_, error = mypy.api.run([str(module), str(fixture)])
-    assert not error
+    if do_assert:
+        assert not error
+    else:
+        print(*_)
+
+
+if __name__ == "__main__":
+    test_typing_mypy(False)
